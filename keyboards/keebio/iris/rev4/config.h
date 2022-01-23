9f17b7f4616dd64c51f3a87eec336ba4fa67e2ae
Ihor <garekus@gmail.com>
2022-01-23 16:14:31 +0200

    garekus layuot

--- keyboards/keebio/iris/rev4/config.h
+++ keyboards/keebio/iris/rev4/config.h
@@ -24,26 +24,22 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MANUFACTURER    Keebio
 #define PRODUCT         Keebio Iris Rev. 4
 
+#define MASTER_LEFT
+#define USE_I2C
+
+
 /* key matrix size */
 // Rows are doubled-up
 #define MATRIX_ROWS 10
 #define MATRIX_COLS 6
 
 // wiring of each half
-#define MATRIX_ROW_PINS { B1, F0, F5, F6, F7 }
-#define MATRIX_COL_PINS { F1, F4, D3, D2, B7, D4 }
-#define MATRIX_ROW_PINS_RIGHT { B1, B2, D2, F1, F4 }
-#define MATRIX_COL_PINS_RIGHT { D4, D7, D3, B7, F0, B3 }
-#define SPLIT_HAND_PIN D5
-#define QMK_ESC_OUTPUT F1
-#define QMK_ESC_INPUT B1
-#define QMK_LED B0
-#define QMK_SPEAKER C6
-
-#define ENCODERS_PAD_A { B2 }
-#define ENCODERS_PAD_B { B3 }
-#define ENCODERS_PAD_A_RIGHT { F7 }
-#define ENCODERS_PAD_B_RIGHT { F6 }
+#define MATRIX_ROW_PINS { F4, F5, F6, F7, B3 }
+#define MATRIX_COL_PINS { B5, B4, E6, D7, C6, B6 } //  B6, C6, D7, E6, B4, B5 }
+#define MATRIX_ROW_PINS_RIGHT { F4, F5, F6, F7, B3 }
+#define MATRIX_COL_PINS_RIGHT { B5, B4, E6, D7, C6, B6 }
+// #define QMK_ESC_OUTPUT F1
+// #define QMK_ESC_INPUT B1
 
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
@@ -57,19 +53,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Set 0 if debouncing isn't needed */
 #define DEBOUNCE 5
 
-/* serial.c configuration for split keyboard */
-#define SOFT_SERIAL_PIN D0
 
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-#define LOCKING_SUPPORT_ENABLE
-/* Locking resynchronize hack */
-#define LOCKING_RESYNC_ENABLE
-
-#define BACKLIGHT_PIN B5
-#define BACKLIGHT_LEVELS 5
-
-/* ws2812 RGB LED */
-#define RGB_DI_PIN D6
-#define RGBLED_NUM 12    // Number of LEDs
-#define RGBLED_SPLIT { 6, 6 }
-#define RGBLIGHT_ANIMATIONS
